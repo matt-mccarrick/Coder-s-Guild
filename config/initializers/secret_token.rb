798854afcc9b1931e1590fc2e92c3cb7d786872f
Matt <matt@ubuntu.ubuntu-domain>
2011-01-17 21:10:26 -0500

    -adding all files to the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodersGuild::Application.config.secret_token = 'df5db253cfedc1313212b8178a708b7b306f1d4fcb49b394145963c8ca1c19a2debc63a1832b83cfc56191c38f3d962c367e0b96ddfd95fecd94aaacfeb16480'
